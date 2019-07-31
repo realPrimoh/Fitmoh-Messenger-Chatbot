98286d49f6461cd8037b310189286e9a45b2b9b4
Priyam Mohanty <priyam.mohanty@berkeley.edu>
2019-07-30 23:43:17 -0700

    first commit

---
+++ keys.js
@@ -0,0 +1,17 @@
+{
+  "name": "fitmoh-messenger",
+  "version": "1.0.0",
+  "description": "Fitmoh Chatbot on Facebook Messenger",
+  "main": " ",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "Priyam Mohanty",
+  "license": "ISC",
+  "dependencies": {
+    "body-parser": "^1.18.3",
+    "express": "^4.16.3",
+    "npm": "^6.1.0",
+    "request": "^2.87.0"
+  }
+}
